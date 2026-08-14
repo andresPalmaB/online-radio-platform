3219cfe09f62cce0ff25024c32b9aa10aa17f2cc
andresPalmaB <palmabustosandres@gmail.com>
2026-08-14 10:15:08 -0500

    chore: initialize Spring Boot backend

---
+++ backend/src/main/java/com/onlineradioplatform/radioapi/RadioApiApplication.java
@@ -0,0 +1,13 @@
+package com.onlineradioplatform.radioapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RadioApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RadioApiApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/onlineradioplatform/radioapi/RadioApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onlineradioplatform.radioapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RadioApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
